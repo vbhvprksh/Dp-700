b4153a30b72ed098ec9d78ce07f106feeb93f4cf
Vaibhav Prakash <58112154+vbhvprksh@users.noreply.github.com>
2026-01-16 17:59:43 +0530

    Committing 9 items from workspace 1c761afd-85aa-44d7-a7d9-95bf3b9ff1bf

---
+++ 007_Medallion_Architecture/Medallion_architecture.Notebook/notebook-content.py
@@ -0,0 +1,175 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "55073f31-916b-4d61-8634-f0c6f4e9c439",
+# META       "default_lakehouse_name": "medallion_architecture",
+# META       "default_lakehouse_workspace_id": "1c761afd-85aa-44d7-a7d9-95bf3b9ff1bf",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "55073f31-916b-4d61-8634-f0c6f4e9c439"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+df = spark.read.format("csv").option("header","true").load("Files/Bronze Layer/2019.csv")
+# df now is a Spark DataFrame containing CSV data from "Files/Bronze Layer/2019.csv".
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.types import *
+    
+# Create the schema for the table
+orderSchema = StructType([
+    StructField("SalesOrderNumber", StringType()),
+    StructField("SalesOrderLineNumber", IntegerType()),
+    StructField("OrderDate", DateType()),
+    StructField("CustomerName", StringType()),
+    StructField("Email", StringType()),
+    StructField("Item", StringType()),
+    StructField("Quantity", IntegerType()),
+    StructField("UnitPrice", FloatType()),
+    StructField("Tax", FloatType())
+    ])
+    
+# Import all files from bronze folder of lakehouse
+df = spark.read.format("csv").option("header", "false").schema(orderSchema).load("Files/bronze/2019.csv")
+    
+# Display the first 10 rows of the dataframe to preview your data
+display(df.head(10))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import when, lit, col, current_timestamp, input_file_name
+    
+# Add columns IsFlagged, CreatedTS and ModifiedTS
+df = df.withColumn("FileName", input_file_name()) \
+    .withColumn("IsFlagged", when(col("OrderDate") < '2019-08-01',True).otherwise(False)) \
+    .withColumn("CreatedTS", current_timestamp()).withColumn("ModifiedTS", current_timestamp())
+    
+# Update CustomerName to "Unknown" if CustomerName null or empty
+df = df.withColumn("CustomerName", when((col("CustomerName").isNull() | (col("CustomerName")=="")),lit("Unknown")).otherwise(col("CustomerName")))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Define the schema for the sales_silver table
+    
+from pyspark.sql.types import *
+from delta.tables import *
+    
+DeltaTable.createIfNotExists(spark) \
+    .tableName("dbo.sales_silver") \
+    .addColumn("SalesOrderNumber", StringType()) \
+    .addColumn("SalesOrderLineNumber", IntegerType()) \
+    .addColumn("OrderDate", DateType()) \
+    .addColumn("CustomerName", StringType()) \
+    .addColumn("Email", StringType()) \
+    .addColumn("Item", StringType()) \
+    .addColumn("Quantity", IntegerType()) \
+    .addColumn("UnitPrice", FloatType()) \
+    .addColumn("Tax", FloatType()) \
+    .addColumn("FileName", StringType()) \
+    .addColumn("IsFlagged", BooleanType()) \
+    .addColumn("CreatedTS", DateType()) \
+    .addColumn("ModifiedTS", DateType()) \
+    .execute()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM medallion_architecture.dbo.sales_silver LIMIT 1000")
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Update existing records and insert new ones based on a condition defined by the columns SalesOrderNumber, OrderDate, CustomerName, and Item.
+
+from delta.tables import *
+    
+deltaTable = DeltaTable.forPath(spark, 'Tables/sales_silver')
+    
+dfUpdates = df
+    
+deltaTable.alias('silver') \
+  .merge(
+    dfUpdates.alias('updates'),
+    'silver.SalesOrderNumber = updates.SalesOrderNumber and silver.OrderDate = updates.OrderDate and silver.CustomerName = updates.CustomerName and silver.Item = updates.Item'
+  ) \
+   .whenMatchedUpdate(set =
+    {
+          
+    }
+  ) \
+ .whenNotMatchedInsert(values =
+    {
+      "SalesOrderNumber": "updates.SalesOrderNumber",
+      "SalesOrderLineNumber": "updates.SalesOrderLineNumber",
+      "OrderDate": "updates.OrderDate",
+      "CustomerName": "updates.CustomerName",
+      "Email": "updates.Email",
+      "Item": "updates.Item",
+      "Quantity": "updates.Quantity",
+      "UnitPrice": "updates.UnitPrice",
+      "Tax": "updates.Tax",
+      "FileName": "updates.FileName",
+      "IsFlagged": "updates.IsFlagged",
+      "CreatedTS": "updates.CreatedTS",
+      "ModifiedTS": "updates.ModifiedTS"
+    }
+  ) \
+  .execute()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
